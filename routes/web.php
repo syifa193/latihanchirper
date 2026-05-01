92a9f0e31709f386e0e87fe93058ba6e93b633ed
Syifa <syifa.220170193@mhs.unimal.ac.id>
2026-05-01 18:09:56 +0700

    add root route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\ChirpController;
 
 Route::get('/chirps', [ChirpController::class, 'index']);
+Route::get('/', function () {
+    return redirect('/chirps');
+});
